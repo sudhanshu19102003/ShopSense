5760c4cdf3a6ecafc230e7ff2a351eecdcaa7fb5
sudhanshu-19102003 <sudhanshu19102003@gmail.com>
2023-07-28 11:52:04 +0530

    add new data in scraping.py

--- Backend/scraping.py
+++ Backend/scraping.py
@@ -13,20 +13,31 @@ def get_amazon_product_data(url):
         product_title = soup.select_one("span#productTitle")
         if product_title:
             product_title = product_title.get_text().strip()
+        
+        #Scrape product version selection
+        product_v = soup.select('div.a-section table tr')
+        if product_v:
+            for row in product_v:
+                cells = row.select('td')
+                if len(cells) == 2:
+                    key = cells[0].text.strip()
+                    value = cells[1].text.strip()
+                    product_v[key] = value
 
         # Scrape "About this item" section
         about_section = soup.select_one("div#feature-bullets ul")
         if about_section:
             about_text = "\n".join([item.get_text().strip() for item in about_section.find_all("li")])
 
-        # Scrape top 10 comments
+        # Scrape top comments
         top_comments = soup.select("div[data-hook='review-collapsed']")
         top_comments = [comment.get_text().strip() for comment in top_comments[:50]]
 
         return {
             "product_title": product_title,
+            "product_title": product_v,
             "about_section": about_text,
-            "top_10_comments": top_comments
+            "top_comments": top_comments
         }
     
     else:
