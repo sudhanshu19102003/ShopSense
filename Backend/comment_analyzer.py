3fdde33db40a3ef62e36b81a531d4313aaa28151
sudhanshu-19102003 <sudhanshu19102003@gmail.com>
2023-07-29 13:49:59 +0530

    new file:   Backend/comment_analyzer.py

---
+++ Backend/comment_analyzer.py
@@ -0,0 +1,5 @@
+# this should have an funtion which has the input lookes like this [comment0,comment1,.,...]
+#some usefull links https://huggingface.co/Sarwar242/autotrain-fake-reviews-labelling-37433101195
+#the funtion must output an graph in html
+
+
