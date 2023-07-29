e0b71aad5d2081876fac2bbc192871a2517eb420
SWAX10 <110764543+swax10@users.noreply.github.com>
2023-07-29 22:46:44 +0530

    Update comment_analyzer.py

--- Backend/comment_analyzer.py
+++ Backend/comment_analyzer.py
@@ -1,5 +1,24 @@
 # this should have an funtion which has the input lookes like this [comment0,comment1,.,...]
 #some usefull links https://huggingface.co/Sarwar242/autotrain-fake-reviews-labelling-37433101195
 #the funtion must output an graph in html
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import os
 
+class model():
+    def __init__(self):       
+        """
+        Model setup
+        
+        """
+        try:
+            self.model = AutoModelForSequenceClassification.from_pretrained("./model")
+        except:
+            print("Downloading model...")
+            os.system("git clone https://huggingface.co/Sarwar242/autotrain-fake-reviews-labelling-37433101195")
+            os.rename("autotrain-fake-reviews-labelling-37433101195", "model")
+            self.model = AutoModelForSequenceClassification.from_pretrained("./model")
 
+        self.tokenizer = AutoTokenizer.from_pretrained("./model")
+
+    def tokenize(self, text):
+        return self.tokenizer(text, return_tensors="pt")
