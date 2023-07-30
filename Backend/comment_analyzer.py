9f6affc6c6cea5ecc570301d455746d737477bea
swax10 <shreyasw496@gmail.com>
2023-07-30 20:42:22 +0530

    Added Docstring

--- Backend/comment_analyzer.py
+++ Backend/comment_analyzer.py
@@ -6,8 +6,19 @@ import os
 
 class model():
     def __init__(self):       
+        pass
+    
+    def setup(self,hf_repo:str):
         """
-        Model setup
+        It sets up HF model
+
+        It downloads the model repo from HF and saves it and rename it as ./model in the current directory.
+
+        Args:
+                hf_repo (str): HF repo name
+
+        Returns:
+                None
         
         """
         try:
@@ -15,11 +26,26 @@ class model():
         except:
             print("Error model not found.")
             print("Downloading model...")
-            os.system("git clone https://huggingface.co/Sarwar242/autotrain-fake-reviews-labelling-37433101195")
-            os.rename("autotrain-fake-reviews-labelling-37433101195", "model")
+            os.system("git clone https://huggingface.co/", hf_repo)
+            hf_repo = hf_repo.split("/")[1]
+            os.rename(hf_repo, "model")
             self.model = AutoModelForSequenceClassification.from_pretrained("./model")
 
-        self.tokenizer = AutoTokenizer.from_pretrained("./model")
 
-    def tokenize(self, text):
+        self.tokenizer = AutoTokenizer.from_pretrained("./model")
+        return None
+
+    def tokenize(self, text:str):
+        """
+        It tokenizes the text
+        
+        It takes top_comments from scrape_website() and tokenizes them.
+
+        Args:
+                text (str): text to be tokenized
+
+        Returns:
+                dict: tokenized text
+        
+        """
         return self.tokenizer(text, return_tensors="pt")
